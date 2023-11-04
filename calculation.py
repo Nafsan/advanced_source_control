1910cc95bef62a852257c87a865d71afd0112fc6
Md. Shafiqul Islam <shafiqul.islam@enosisbd.com>
2023-11-04 22:37:03 +0600

    Add subtraction of two numbers

--- calculation.py
+++ calculation.py
@@ -2,3 +2,4 @@ first_number = 3
 second_number = 5
 
 print(f'{first_number} + {second_number} = {first_number + second_number}')
+print(f'{first_number} - {second_number} = {first_number - second_number}')
