46bcdd3282c4a9c72e746726665278720ff7078c
Md. Shafiqul Islam <shafiqul.islam@enosisbd.com>
2023-11-04 22:39:33 +0600

    Add division of two numebers (Introduced a bug)

--- calculation.py
+++ calculation.py
@@ -1,6 +1,7 @@
-first_number = 3
-second_number = 5
+first_number = int(input())
+second_number = int(input())
 
 print(f'{first_number} + {second_number} = {first_number + second_number}')
 print(f'{first_number} - {second_number} = {first_number - second_number}')
 print(f'{first_number} * {second_number} = {first_number * second_number}')
+print(f'{first_number} / {second_number} = {first_number / second_number}')
